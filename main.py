fe375fbc8f24c1c0c6228de5f567176bf93b4897
gpq8l8s <gpq8l8s@gmail.com>
2023-04-29 15:57:34 +0200

    added comment and deleted not used files

--- main.py
+++ main.py
@@ -5,28 +5,36 @@ from flask import render_template
 
 app = Flask(__name__)
 
+# Get JSON data
 with open("news_url.json", "r", encoding="utf-8") as url_lists:
     article = json.load(url_lists)
 
-# HOME
+
+# route to create main page
 @app.route('/')
 def main():
     return render_template('main.html')
 
-# BBC
+# route to create BBC page
 @app.route("/BBC")
 def get_bbc():
-        feeds = feedparser.parse(article["BBC"])
-        all_the_news = []
-        for content in feeds['entries']:
-            all_the_news.append(content)
-        return render_template('bbc.html', all_the_news=all_the_news)
+    """
+        Function to get data of BBC rss page.
+        Created an empty list to add all the datas
+        and rendered it
+    """
+    feeds = feedparser.parse(article["BBC"])
+    all_the_news = []
+    for content in feeds['entries']:
+        all_the_news.append(content)
+    return render_template('bbc.html', all_the_news=all_the_news)
 
-# CNN
+# route to create CNN page
 @app.route("/CNN")
 def show_cnn():
         """
-        
+        Function to get data of CNN rss page.
+        Same with the function of BBC
         """
         feeds = feedparser.parse(article["CNN"])
         all_the_news = []
